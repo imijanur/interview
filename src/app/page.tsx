5626a3fb1c55d70ea7aa16883608945d253da01f
Mijanur Rahaman <mijanurtj@gmail.com>
2024-08-20 07:59:52 +0530

    App functionality initially complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,200 @@
-import Image from "next/image";
+'use client'
+
+import { FormEvent, useState } from "react";
 
 export default function Home() {
+  const [matrixA, setMatrixA] = useState<number[][]>([]);
+  const [matrixB, setMatrixB] = useState<number[][]>([]);
+  const [result, setResult] = useState<number[][]>([]);
+  const [generated, setGenerated] = useState(false);
+  const [calculated, setCalculated] = useState(false);
+  const [numRows, setNumRows] = useState(4);
+  const [numColumns, setNumColumns] = useState(4);
+
+  const addMatrices = () => {
+    const result = matrixA.map((row, i) =>
+      row.map((val, j) => val + matrixB[i][j])
+    );
+    setResult(result);
+    setCalculated(true);
+  };
+
+  const subtractMatrices = () => {
+    const result = matrixA.map((row, i) =>
+      row.map((val, j) => val - matrixB[i][j])
+    );
+    setResult(result);
+    setCalculated(true);
+  };
+
+  const multiplyMatrices = () => {
+    const result = matrixA.map((row, i) =>
+      row.map((val, j) => val * matrixB[i][j])
+    );
+    setResult(result);
+    setCalculated(true);
+  };
+
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    matrix: number[][],
+    setMatrix: React.Dispatch<React.SetStateAction<number[][]>>,
+    row: number,
+    col: number
+  ) => {
+    const newMatrix = matrix.map((r, i) =>
+      r.map((val, j) => (i === row && j === col ? Number(e.target.value) : val))
+    );
+    setMatrix(newMatrix);
+  };
+
+  const haandleGenerateMatrix = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // generate matrix A
+    const matrixA = Array.from({ length: numRows }, (_,i) => Array.from({ length: numColumns }, (_, j) => i+j))
+    setMatrixA(matrixA);
+    // generate matrix B
+    const matrixB = Array.from({ length: numRows }, (_,i) => Array.from({ length: numColumns }, (_, j) => i*j))
+    setMatrixB(matrixB);
+    setGenerated(true);
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen flex-col items-center">
+      <form action="" onSubmit={haandleGenerateMatrix} className="flex gap-4">
+        <div className="flex flex-col justify-center">
+          <label htmlFor="rows" className="text-lg font-semibold">
+            Rows
+          </label>
+          <input
+            type="number"
+            id="rows"
+            name="rows"
+            value={numRows}
+            onChange={(e) => setNumRows(parseInt(e.target.value))}
+            className="border border-gray-300 rounded-md w-20 p-2"
+          />
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div className="flex flex-col justify-center">
+          <label htmlFor="columns" className="text-lg font-semibold">
+            Columns
+          </label>
+          <input
+            type="number"
+            id="columns"
+            name="columns"
+            value={numColumns}
+            onChange={(e) => setNumColumns(parseInt(e.target.value))}
+            className="border border-gray-300 w-20 rounded-md p-2"
+          />
+        </div>
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-4  py-2 rounded-md mt-4"
         >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+          Generate Matrix
+        </button>
+      </form>
+      <hr />
+      
+      {generated && <div className="flex gap-4">
+        {/* // addition matrix input */}
+        <div className="flex flex-col items-center gap-4">
+          <h2 className="text-lg font-semibold">Matrix A</h2>
+          <div className="flex">
+            <div className="flex flex-col gap-4 items-center">
+              {matrixA.map((row, i) => (
+                <div key={i} className="flex flex-col gap-4 items-center">
+                  <div>
+                    {row.map((col, j) => (
+                      <input
+                        type="number"
+                        key={j}
+                        onChange={(e) => handleInputChange(e, matrixA, setMatrixA, i, j)}
+                        className="border w-16 border-gray-300 rounded-md p-2"
+                        value={col}
+                      />
+                    ))}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        {/* Multiply matrix */}
+        <div className="flex flex-col items-center gap-4">
+          <h2 className="text-lg font-semibold">Matrix B</h2>
+          <div className="flex">
+            <div className="flex flex-col gap-4 items-center">
+              {matrixB.map((row, i) => (
+                <div key={i} className="flex flex-col gap-4 items-center">
+                  <div>
+                    {row.map((col, j) => (
+                      <input
+                        type="number"
+                        key={j}
+                        onChange={(e) => handleInputChange(e, matrixB, setMatrixB, i, j)}
+                        className="border w-16 border-gray-300 rounded-md p-2"
+                        value={col}
+                      />
+                    ))}
+                  </div>
+                </div>
+              ))}
+            </div>
+            </div>
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        <hr />
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+
+      </div>}
+
+      {generated && <div className="flex gap-4 px-8">
+        <hr />
+          <button
+            onClick={addMatrices}
+            className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4"
+          >
+            Add
+          </button>
+          <button
+            onClick={subtractMatrices}
+            className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4"
+          >
+            Subtract
+          </button>
+          <button
+            onClick={multiplyMatrices}
+            className="bg-blue-500 text-white px-4 py-2 rounded-md mt-4"
+          >
+            Multiply
+          </button>
+        </div>}
+
+      {calculated && <div className="flex mt-8 gap-4">
+        {/* Result matrix */}
+        <div className="flex flex-col items-center gap-4">
+          <h2 className="text-lg font-semibold">Result</h2>
+          <div className="flex">
+            <div className="flex flex-col gap-4 items-center">
+              {result.map((row, i) => (
+                <div key={i} className="flex flex-col gap-4 items-center">
+                  <div className="flex">
+                    {row.map((col, j) => (
+                      <div
+                        key={j}
+                        className="border text-center w-16 border-gray-300 rounded-md p-2"
+                      > {col}</div>
+                    ))}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        </div>}
+
     </main>
   );
 }
